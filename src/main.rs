672983192166989576b3ec3f55fbec6cff1b88c9
admin <3052405886@qq.com>
2025-08-20 21:15:11 +0800

    增加前端页面

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use axum::Router;
+use askama::Template;
+use axum::{debug_handler, Router};
+use axum::response::Html;
 use axum::routing::get;
 use tokio::net::TcpListener;
 use utils::database;
@@ -10,11 +12,29 @@ mod utils;
 async fn main() -> anyhow::Result<()> {
     logger::init();
     database::init().await?;
-    let routr = Router::new().route("/", get(|| async { "Hello, world!" }));
+    let routr = Router::new().route("/", get(index));
     let server_config = config::get().server_config();
     let host = format!("{}:{}", server_config.host(), server_config.port());
     let listener = TcpListener::bind(host).await?;
     tracing::info!("Listening on {}", listener.local_addr()?);
     axum::serve(listener, routr).await?;
     Ok(())
+}
+
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct Index{
+    name: String
+}
+
+
+#[debug_handler]
+async fn index() -> Html<String> {
+    let template = Index{name: "index.html".to_string()};
+
+    match template.render() {
+        Ok(rendered) => Html(rendered),
+        Err(e) => Html(format!("Error rendering template: {}", e)),
+    }
 }
