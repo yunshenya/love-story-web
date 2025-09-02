9652a67fb285666cc892c053af64fcabc56aff06
yunshen <yunshen4869@gmail.com>
2025-09-02 19:29:26 +0800

    新增登陆接口

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -3,19 +3,21 @@ use crate::api::users::create_user_router;
 use crate::handler::index::index;
 use crate::server::app::AppState;
 use axum::Router;
+use axum::routing::get_service;
 use tower_http::services::{ServeDir, ServeFile};
 
 mod users;
 mod auth;
 
 pub fn create_router() -> Router<AppState> {
-    // 使用相对路径而不是绝对路径
+
     let service_dir = ServeDir::new("static");
 
     Router::new()
         .route("/", axum::routing::get(index))
         .nest("/api", Router::new().nest("/user", create_user_router()))
         .nest("/api/auth", auth_routes())
+        .route_service("/favicon.ico",  get_service(ServeFile::new("static/favicon.ico")))
         .nest_service("/static", service_dir)
-        .fallback(axum::routing::get_service(ServeFile::new("templates/404.html")))
+        .fallback(get_service(ServeFile::new("templates/404.html")))
 }
